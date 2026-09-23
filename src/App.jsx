a8246fc61b62cebf4e217a9ac1331cb9e68b656c
sewak07 <sew007ak@gmail.com>
2026-09-23 20:26:54 +0545

    Fix GitHub Pages base path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ScrollToTop from './components/layout/ScrollToTop';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
